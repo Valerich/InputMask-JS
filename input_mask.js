ac17f738f656e5c689599157e9826ec261553841
Bair <valerievich.19@yandex.ru>
2023-12-14 19:24:04 +0700

    number validation

--- input_mask.js
+++ input_mask.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   let telInput = document.querySelectorAll("input[data-phone-input]");
+  let phoneInput = document.getElementById("phone");
 
   let getInputNumbersValue = function (input) {
     return input.value.replace(/\D/g, ""); // Получение только числовых значений
@@ -13,11 +14,11 @@ document.addEventListener("DOMContentLoaded", function () {
       return (input.value = "");
     }
     // Проверяем начало номера
-    if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+    if (["7", "8", "9"].includes(inputNumbersValue[0])) {
       if (inputNumbersValue[0] == "9") {
         inputNumbersValue = "7" + inputNumbersValue;
       }
-      let firstSymbols = inputNumbersValue[0] == "8" ? "8" : "+7";
+      let firstSymbols = inputNumbersValue[0] == "8" ? " 8" : "+7";
       formattedInputValue = firstSymbols + " ";
       // Форматируем вид номера
       if (inputNumbersValue.length > 1) {
@@ -33,22 +34,34 @@ document.addEventListener("DOMContentLoaded", function () {
         formattedInputValue += "-" + inputNumbersValue.substring(9, 11);
       }
     } else {
-      formattedInputValue = '+' + inputNumbersValue;
+      formattedInputValue = "+" + inputNumbersValue;
     }
     input.value = formattedInputValue;
+    phoneInput.classList.remove("error");
+  };
+
+  // Class error при недостаточной длине номера
+  let validatePhoneNumber = function (e) {
+    let inputValue = e.target.value;
+    if (inputValue.length > 0 && inputValue.length < 18) {
+      phoneInput.classList.add("error");
+    } else {
+      phoneInput.classList.remove("error");
+    }
   };
 
   // Возможность удаления первого символа в поле ввода
   let onPhoneKeyDown = function (e) {
-    let inputValue = e.target.value.replace(/\D/g, '');
+    let inputValue = e.target.value.replace(/\D/g, "");
     if (e.keyCode == 8 && inputValue.length == 1) {
-        e.target.value = "";
+      e.target.value = "";
     }
-}
+  };
   // Обрабодка вводимых значений в поле ввода
   for (i = 0; i < telInput.length; i++) {
     let input = telInput[i];
     input.addEventListener("input", onTelInput);
-    input.addEventListener('keydown', onPhoneKeyDown);
+    input.addEventListener("keydown", onPhoneKeyDown);
+    input.addEventListener("blur", validatePhoneNumber);
   }
 });
